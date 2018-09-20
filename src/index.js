3c5208875cea3e32332cad3954c74b83fa1ddb98
Davidovich Dmitry <42535493+dmitryDuk3@users.noreply.github.com>
2018-09-20 16:02:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function warmup(temperature) {
-  let fahrenheit = celsius * (9 / 5) + 32;// your implementation here
+  let fahrenheit = temperature * (9 / 5) + 32;// your implementation here
   fahrenheit = Math.floor(fahrenheit);
   return fahrenheit;
 };
